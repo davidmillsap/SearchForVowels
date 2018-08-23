fd184b126211f6de224907d48db5c7efb93a186f
David <davemillsap@gmail.com>
2018-08-23 11:09:12 -0700

    feat_databaseconn

--- hello_flask.py
+++ hello_flask.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
+from DBcm import UseDatabase
+
 
 app = Flask(__name__)
 
+app.config['dbconfig'] = {'host' : '127.0.0.1',
+                          'user' : 'vsearch',
+                          'password' : 'vsearchpasswd',
+                          'database' : 'vsearchlogDB', }
+
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+    """Log details of the web request and the results."""
+
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
+                (phrase, letters, ip, browser_string, results)
+                values
+                (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                        req.form['letters'],
+                        req.remote_addr,
+                        req.user_agent.browser,
+                        res, ))
 
 
 @app.route('/search4', methods=['POST'])
@@ -29,13 +46,12 @@ def entry_page() -> 'html':
 
 @app.route('/viewlog')
 def view_the_log() -> 'html':
-    contents = []
-    with open('vsearch.log') as log:
-        for line in log:
-            contents.append([])
-            for item in line.split('|'):
-                contents[-1].append(escape(item))
-    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """SELECT phrase, letters, ip, browser_string, results
+               from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+    titles = ('Phrase', 'Letters', 'Remote_addr', 'User-agent', 'Results')
     return render_template('viewlog.html',
                             the_title = 'View Log',
                             the_row_titles=titles,
